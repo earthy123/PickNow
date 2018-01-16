8c610a38f2ba2cc2ecf9dd3e0f9f896d12b66d77
Unknown <step.earthz@gmail.com>
2018-01-16 15:56:56 +0700

    cut file support

--- upload.py
+++ upload.py
@@ -5,37 +5,34 @@ Created on Mon Jan 15 16:37:20 2018
 
 @author: earthz
 """
-
+import requests
 import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-
+import sys
 UPLOAD_FOLDER = 'img'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
+ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
+url = 'https://pic.azurewebsites.net/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def api(filename):
+    files = {'file': open(filename, 'rb')}
+    requests.post(url, files=files)
            
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-#        if 'file' not in request.files:
-#            flash('No file part')
-#            return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-#        if file.filename == '':
-#            flash('No selected file')
-#            return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+#            print(filename, file=sys.stderr)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            api(filename)
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(port='5000')
+    app.run(host='0.0.0.0',port='5000')
