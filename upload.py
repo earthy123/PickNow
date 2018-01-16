5bfc2f502ea16e3a114e2e4c1dc2cad266ca2524
Unknown <step.earthz@gmail.com>
2018-01-16 23:45:59 +0700

    tess

--- upload.py
+++ upload.py
@@ -31,11 +31,10 @@ def upload_file():
             filename = secure_filename(file.filename)
 #            print(filename, file=sys.stderr)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            api(filename)
+#            api(filename)
     return render_template('index.html')
 
 
-if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 5000))
+if __name__ == '__main__':
     app.run(debug=True)
-    app.run(port='5000')
+    app.run(host='0.0.0.0',port='5000')
