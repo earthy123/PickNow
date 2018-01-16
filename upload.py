1bdde8cb4b69704e42ea3cfe437f902ea9fecb29
Unknown <step.earthz@gmail.com>
2018-01-16 16:28:52 +0700

    commit 14

--- upload.py
+++ upload.py
@@ -5,25 +5,15 @@ Created on Mon Jan 15 16:37:20 2018
 
 @author: earthz
 """
-import requests
+
 import os
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-import sys
+
 UPLOAD_FOLDER = 'img'
-<<<<<<< HEAD
-<<<<<<< HEAD
 CUTBG_FOLDER ='cbg'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-=======
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
-url = 'https://pic.azurewebsites.net/'
->>>>>>> master
-=======
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
-url = 'https://pic.azurewebsites.net/'
->>>>>>> master
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -38,26 +28,15 @@ def allowed_file(filename):
 def api(filename):
     files = {'file': open(filename, 'rb')}
     requests.post(url, files=files)
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 @app.route('/upload', methods=['POST'])
-=======
-=======
->>>>>>> master
-           
-@app.route('/', methods=['GET', 'POST'])
->>>>>>> master
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-#            print(filename, file=sys.stderr)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             api(filename)
-<<<<<<< HEAD
-<<<<<<< HEAD
     return render_template('index2.html', image_name=filename)
 
 @app.route('/finish')
@@ -72,11 +51,6 @@ def send_image(filename):
 
 # @app.route('')
 
-=======
-=======
->>>>>>> master
-    return render_template('index.html')
->>>>>>> master
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host='0.0.0.0',port='5000')
+    app.run(port='5000')
