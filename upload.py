f149635ed1978f472d3c64a38d156bf4f5de2a5c
Unknown <step.earthz@gmail.com>
2018-01-16 23:54:55 +0700

    test commit

--- upload.py
+++ upload.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 15 16:37:20 2018
- 
 @author: earthz
 """
 import requests
@@ -12,13 +11,13 @@ from werkzeug.utils import secure_filename
 import sys
 UPLOAD_FOLDER = 'img'
 ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
-url = 'http://52.168.49.231:5000/'
+url = 'https://pic.azurewebsites.net/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
- 
+
 def api(filename):
     files = {'file': open(filename, 'rb')}
     requests.post(url, files=files)
@@ -31,10 +30,8 @@ def upload_file():
             filename = secure_filename(file.filename)
 #            print(filename, file=sys.stderr)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#            api(filename)
+            api(filename)
     return render_template('index.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
     app.run(host='0.0.0.0',port='5000')
