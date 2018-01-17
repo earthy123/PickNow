c35d5bbb57369f5f916425540bf631a6eb27bba3
Unknown <step.earthz@gmail.com>
2018-01-17 13:47:10 +0700

    fk

--- upload.py
+++ upload.py
@@ -1,39 +1,18 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 15 16:37:20 2018
-@author: earthz
-"""
-import requests
-import os
-from flask import Flask, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
-import sys
-UPLOAD_FOLDER = 'img'
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
-url = 'https://pic.azurewebsites.net/'
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def api(filename):
-    files = {'file': open(filename, 'rb')}
-    requests.post(url, files=files)
-           
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+
+
+
+app = Flask(__name__)
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-#            print(filename, file=sys.stderr)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            api(filename)
-    return render_template('index.html')
 
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
