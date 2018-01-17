2cb125789bfd6ef7c27cd016542855c052e98908
Unknown <step.earthz@gmail.com>
2018-01-17 14:48:43 +0700

    last last

--- upload.py
+++ upload.py
@@ -10,7 +10,9 @@ import sys
 
 url = 'http://52.168.73.191:5000/'
 UPLOAD_FOLDER = './img/'
+BG_FOLDER = './bgimg/'
 app = Flask(__name__)
+app.config['BG_FOLDER'] = BG_FOLDER
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
 def allowed_file(filename):
@@ -23,14 +25,23 @@ def api(filename):
     
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-#            print(filename, file=sys.stderr)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            api(filename)
-            print("send img", file=sys.stderr)
+    if request.method == 'POST' and 'file' in request.files:
+            file=request.files['file']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+    #            print(filename, file=sys.stderr)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                api(filename)
+                print("send img", file=sys.stderr)
+            
+    elif request.method == 'POST' and 'file_bg' in request.files:
+            file=request.files['file_bg']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+    #            print(filename, file=sys.stderr)
+                file.save(os.path.join(app.config['BG_FOLDER'], filename))
+
+                print("send img_bg", file=sys.stderr)
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
