510805c13910d2780a2bd8171ea28b6aa23d0672
Unknown <step.earthz@gmail.com>
2018-01-17 13:50:38 +0700

    sus request

--- upload.py
+++ upload.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request
+import requests
+import os
+from flask import Flask, request, redirect, url_for, render_template
 from flask_uploads import UploadSet, configure_uploads, IMAGES
-
-
-
+from werkzeug.utils import secure_filename
+import sys
 
 app = Flask(__name__)
-
+ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+#            print(filename, file=sys.stderr)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
