80956968d2ca80826f3d20138f4ae00bd14e70e7
Unknown <step.earthz@gmail.com>
2018-01-17 14:19:37 +0700

    mun work

--- upload.py
+++ upload.py
@@ -9,7 +9,7 @@ from werkzeug.utils import secure_filename
 import sys
 
 url = 'http://52.168.73.191:5000/'
-UPLOAD_FOLDER = 'img'
+UPLOAD_FOLDER = './img/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
@@ -18,7 +18,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
            
 def api(filename):
-    files = {'file': open(filename, 'rb')}
+    files = {'file': open(UPLOAD_FOLDER+filename, 'rb')}
     requests.post(url, files=files)
     
 @app.route('/', methods=['GET', 'POST'])
@@ -29,8 +29,8 @@ def upload_file():
             filename = secure_filename(file.filename)
 #            print(filename, file=sys.stderr)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#            api(filename)
-
+            api(filename)
+            print("send img", file=sys.stderr)
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
