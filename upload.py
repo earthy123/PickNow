4bcf721fbc5e7b8191b0891c3edcc27b8077ccf2
Unknown <step.earthz@gmail.com>
2018-01-17 15:22:05 +0700

    sdsdsd

--- upload.py
+++ upload.py
@@ -8,17 +8,19 @@ from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import requests
 
-#
-#url = 'http://52.168.73.191:5000/'
-#
-#def api(filename):
-#    files = {'file': open(UPLOAD_FOLDER+filename, 'rb')}
-#    requests.post(url, files=files)
+UPLOAD_FOLDER = './img/'
+url = 'http://52.168.73.191:5000/'
+a=UPLOAD_FOLDER+'matthias-clamer.jpeg'
+def api(filename):
+    files = {'file': open(UPLOAD_FOLDER+filename, 'rb')}
+    requests.post(url, files=files)
     
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def ss():
+    api(a)
+    
     return "Hi"
 if __name__ == '__main__':
     app.run(debug=True)
