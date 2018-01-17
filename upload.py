59c0ca1aee8f8c428318dd216ddf868f85c29539
Unknown <step.earthz@gmail.com>
2018-01-17 13:28:47 +0700

    render template

--- upload.py
+++ upload.py
@@ -6,31 +6,35 @@ import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import sys
-UPLOAD_FOLDER = 'img'
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
-url = 'https://pic.azurewebsites.net/'
+#UPLOAD_FOLDER = 'img'
+#ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
+#url = 'https://pic.azurewebsites.net/'
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def api(filename):
-    files = {'file': open(filename, 'rb')}
-    requests.post(url, files=files)
-           
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-#            print(filename, file=sys.stderr)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            api(filename)
+@app.route('/')
+def upload():
     return render_template('index.html')
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#
+#
+#def allowed_file(filename):
+#    return '.' in filename and \
+#           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+#
+#def api(filename):
+#    files = {'file': open(filename, 'rb')}
+#    requests.post(url, files=files)
+#           
+#@app.route('/', methods=['GET', 'POST'])
+#def upload_file():
+#    if request.method == 'POST':
+#        file = request.files['file']
+#        if file and allowed_file(file.filename):
+#            filename = secure_filename(file.filename)
+##            print(filename, file=sys.stderr)
+#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#            api(filename)
+#    return render_template('index.html')
 
 
 if __name__ == '__main__':
