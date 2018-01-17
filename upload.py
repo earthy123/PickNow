88324fa35b8dd6e5be36cefec72b30311562e274
Unknown <step.earthz@gmail.com>
2018-01-17 15:19:16 +0700

    test

--- upload.py
+++ upload.py
@@ -8,43 +8,18 @@ from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 import requests
 
-
-url = 'http://52.168.73.191:5000/'
-
-def api(filename):
-    files = {'file': open(UPLOAD_FOLDER+filename, 'rb')}
-    requests.post(url, files=files)
+#
+#url = 'http://52.168.73.191:5000/'
+#
+#def api(filename):
+#    files = {'file': open(UPLOAD_FOLDER+filename, 'rb')}
+#    requests.post(url, files=files)
     
-UPLOAD_FOLDER = './img/'
-BG_FOLDER = './bgimg/'
+
 app = Flask(__name__)
-app.config['BG_FOLDER'] = BG_FOLDER
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-           
-
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST' and 'file' in request.files:
-            file=request.files['file']
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                api(filename)
-
-    elif request.method == 'POST' and 'file_bg' in request.files:
-            file=request.files['file_bg']
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-
-                file.save(os.path.join(app.config['BG_FOLDER'], filename))
-
-    return render_template('index.html')
+def ss():
+    return "Hi"
 if __name__ == '__main__':
     app.run(debug=True)
 
