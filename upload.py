8c507c3bf0a9a236c3bf0785812360848234de39
Unknown <step.earthz@gmail.com>
2018-01-19 13:11:35 +0700

    fluke era

--- upload.py
+++ upload.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 16:37:20 2018
 
-
-import os
-from flask import Flask, request, redirect, url_for, render_template,jsonify
+@author: earthz
+"""
 import requests
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-
-
-
 url = 'http://52.168.73.191:5000/'
+UPLOAD_FOLDER = 'img'
+CUTBG_FOLDER =  'bgimg'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-
-    
-UPLOAD_FOLDER = './img/'
-BG_FOLDER = './bgimg/'
 app = Flask(__name__)
-app.config['BG_FOLDER'] = BG_FOLDER
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
+app.config['CUTBG_FOLDER'] = CUTBG_FOLDER
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def api(filename):
     try:
@@ -26,30 +34,30 @@ def api(filename):
         requests.post(url, files=files)
     except:
         pass
-    
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-           
 
-@app.route('/', methods=['GET', 'POST'])
-
-
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST' and 'file' in request.files:
-            file=request.files['file']
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#                api(filename)
+    if request.method == 'POST' and  'file' in request.files:
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            api(filename)
+    elif request.method == 'POST' and  'file_bg' in request.files:
+        file = request.files['file_bg']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['CUTBG_FOLDER'], filename))
+            
+    return render_template('index2.html', image_name=filename)
 
-    elif request.method == 'POST' and 'file_bg' in request.files:
-            file=request.files['file_bg']
-            if file and allowed_file(file.filename):       
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['BG_FOLDER'], filename))
+@app.route('/upload/<filename>')
+def receive_image(filename):
+            # api(filename)
+    return send_from_directory("bgimg",filename)
+
+# @app.route('')
 
 
-    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
